884196d81b261912b9bff9ca569b6818148c2d6e
Alan Peh <alanpeh87@gmail.com>
2024-07-27 21:35:30 +0800

    sns/sqs working, need to add in other details next

--- aws_sqs_sns_27jul/sqs_sns_config.tf
+++ aws_sqs_sns_27jul/sqs_sns_config.tf
@@ -13,5 +13,5 @@ resource "aws_sns_topic" "user_updates" {
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.user_updates.arn
   protocol  = "sqs"
-  endpoint  = aws_sqs_queue.user_updates_queue.arn
+  endpoint  = aws_sqs_queue.c7_alanp_tf_sqscon_27jul.arn
 }
